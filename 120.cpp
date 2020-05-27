5ca98cd30bf0cea1b7f985a70828522eb060bc61
Marfy <1903701470@qq.com>
2020-05-27 16:08:59 +0800

    120 递归

--- 120.cpp
+++ 120.cpp
@@ -4,13 +4,47 @@
 using namespace std;
 
 class Solution {
+private:
+
+    int res;
+
+    void FindMinimum(vector<vector<int>>& triangle, int x, int y, int& sum){
+
+        if(x == triangle.size()){
+            cout<<"res = "<<res<<" sum = "<<sum<<endl;
+            if(sum < res)
+                res = sum;
+            return;
+        }
+
+        cout<<"res = "<<res<<" sum = "<<sum<<" triangle[ "<<x<<", "<<y<<" ] : "<<triangle[x][y]<<endl;
+        for(int i = 0; i < 2; i++){
+            sum += triangle[x][y];
+            FindMinimum(triangle, x+1, y+i, sum);
+            sum -= triangle[x][y];
+        }
+
+        return;
+    }
+
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
+        
+        res = 0;
+        for(int i = 0; i < triangle.size(); i++)
+            res += triangle[i][0];
+        
+        int sum = 0;
 
+        FindMinimum(triangle, 0, 0, sum);
+
+        return res;
     }
 };
 
 int main(){
 
+    vector<vector<int>> triangle{{2}, {3,4}, {6,5,7}, {4,1,8,3}};
+    cout<<Solution().minimumTotal(triangle)<<endl;
     return 0;
 }
