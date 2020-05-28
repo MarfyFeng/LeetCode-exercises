b2b3d0fc0df58a81549911f3e5b816ac169a345b
Marfy <1903701470@qq.com>
2020-05-28 15:26:54 +0800

    120 try

--- 120.cpp
+++ 120.cpp
@@ -6,34 +6,22 @@ using namespace std;
 class Solution {
 private:
 
-    int res;
-    vector<vector<int>> sums;
+    int FindMinimum(vector<vector<int>>& triangle, int x, int y, vector<vector<int>>& sums){
 
-    int FindMinimum(vector<vector<int>>& triangle, int x, int y){
+        int left = FindMinimum(triangle, x+1, y, sums);
+        int right = FindMinimum(triangle, x+1, y+1, sums);
+        sums[x][y] = triangle[x][y] + (left < right ? left : right);
 
-        if(x == triangle.size()-1){
-            memcpy(&sums[x][0], &triangle[x][0], triangle[x].size());
-            return sums[x][y];
-        }
-
-        if(FindMinimum(triangle, x+1, y) < FindMinimum(triangle, x+1, y+1))
-            sums[x][y] = triangle[x][y] + FindMinimum(triangle, x+1, y+i);
- 
         return sums[x][y];
     }
 
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         
-        res = 0;
-        for(int i = 0; i < triangle.size(); i++)
-            res += triangle[i][0];
+        vector<vector<int>> sums;
+
+        FindMinimum(triangle, 0, 0, sums);
         
-        sums.clear();
-
-        FindMinimum(triangle, 0, 0);
-
-        return res;
     }
 };
 
