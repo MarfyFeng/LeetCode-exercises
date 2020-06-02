589930dfc86d2c694807ff07c348fc4f0c7d2d9b
unknown <1903701470@qq.com>
2020-06-03 00:48:56 +0800

    343 动态规划

--- 343.cpp
+++ 343.cpp
@@ -6,33 +6,21 @@
 using namespace std;
 
 class Solution {
-private:
-
-    vector<int> memo;
-
-    int breakInteger(int n){
-
-        if(n == 1)
-            return 1;
-
-        if(memo[n] != -1)
-            return memo[n];
-
-        int res = -1; 
-        for(int i = 1; i <= n-1; i++){
-            res = max(res, max(i*(n-i), i*breakInteger(n-i)));
-        }
-        memo[n] = res;
-        return res;
-    }
-
 public:
     int integerBreak(int n) {
         
         assert(n >= 2);
-        memo = vector<int>(n+1, -1);
+        vector<int> memo(n+1, -1);
 
-        return breakInteger(n);
+        for(int i = 1; i <= n; i++)
+            if(memo[i] == -1)
+                if(i == 1)
+                    memo[i] = 1;
+                else
+                    for(int j = 1; j <= i-1; j++)
+                        memo[i] = max(memo[i], max(j*(i-j), j*memo[i-j]));
+
+        return memo[n];
     }
 };
 
