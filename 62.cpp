d2e2e78631088f0edea66b90217734836bae2d52
unknown <1903701470@qq.com>
2020-06-04 10:32:51 +0800

    62 动态规划

--- 62.cpp
+++ 62.cpp
@@ -4,42 +4,33 @@
 using namespace std;
 
 class Solution{
-private:
-
-    vector<vector<int>> direction{
-        {0,1},
-        {1,0}
-    };
-
-    vector<vector<int>> memo;
-
-    int FindPaths(int m, int n, int x, int y){
-        
-        if(x == m || y == n)
-            return 0;
-
-        if(x == m-1 && y == n-1)
-            return 1;
-
-        if(memo[x][y] != -1)
-            return memo[x][y];
-
-        memo[x][y] = FindPaths(m, n, x+direction[0][0], y+direction[0][1]) + FindPaths(m, n, x+direction[1][0], y+direction[1][1]);
-        
-        return memo[x][y];
-    }
-
 public:
     int uniquePaths(int m, int n){
 
-        memo = vector<vector<int>>(m, vector<int>(n, -1));
+        vector<vector<int>> direction{
+            {0,1},
+            {1,0}
+        };
 
-        return FindPaths(m, n, 0, 0);;
+        vector<vector<int>> memo(m, vector<int>(n, -1));
+
+        for(int i = m-1; i >= 0; i--)
+            memo[i][n-1] = 1;
+
+        for(int j = n-1; j >= 0; j--)
+            memo[m-1][j] = 1;
+
+        for(int i = m-2; i >= 0; i--)
+            for(int j = n-2; j >= 0; j--)
+                if(memo[i][j] == -1)
+                    memo[i][j] = memo[i+direction[0][0]][j+direction[0][1]] + memo[i+direction[1][0]][j+direction[1][1]];
+              
+        return memo[0][0];
     }
 };
 
 int main(){
 
-    cout<<Solution().uniquePaths(3,2)<<endl;
+    cout<<Solution().uniquePaths(7,3)<<endl;
     return 0;
 }
